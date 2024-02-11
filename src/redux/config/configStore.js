18818bbc593ac59d7dec4e1809d4d496629da28f
kimsohyeon <aotoyae@gmail.com>
2024-02-11 18:56:50 +0900

    add: devtools 추가

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,8 +1,9 @@
 import { combineReducers, createStore } from "redux";
+import { devToolsEnhancer } from "redux-devtools-extension";
 import letters from "../modules/letters";
 import member from "../modules/member";
 
 const rootReducer = combineReducers({ letters, member });
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, devToolsEnhancer());
 
 export default store;
